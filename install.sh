5cee4cf14019044ed3b597b5a947f70f8f481e6f
Matt Welch <mattw0308@gmail.com>
2015-06-06 16:52:40 -0600

    fixed path error in install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ln -s ~/daffy-dotfiles/vimrc ~/.vimrc
-ln -s ~/daffy-dotfiles/screenrc ~/.screenrc
+ln -s ./vimrc ~/.vimrc
+ln -s ./screenrc ~/.screenrc
 git config user.name "Matt Welch"
 git config user.email "mattw0308@gmail.com"
-git config editor "vim"
+git config core.editor "vim"
 
