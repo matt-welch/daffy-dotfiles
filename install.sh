dd63e15516728dadb42a6e42ff65dd9da6460e04
Matt Welch <mattw0308@gmail.com>
2015-06-06 16:03:14 -0600

    REALLY added .gitignore and install script to commit :)

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ~/daffy-dotfiles/vimrc ~/.vimrc
+ln -s ~/daffy-dotfiles/screenrc ~/.screenrc
+git config user.name "Matt Welch"
+git config user.email "mattw0308@gmail.com"
+git config editor "vim"
+
