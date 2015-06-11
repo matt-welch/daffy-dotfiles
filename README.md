d7f55b479a4eb7dce389deb9071377f645c1ceaa
Matt Welch <mattw0308@gmail.com>
2015-06-10 22:32:01 -0600

    added --global to git config and instruction to run install.sh to README

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Configuration files for:
 vim
 screen
 
+Run ./install.sh to make symbolic links into $HOME and to set git config parameters
 
--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+ 
 echo Linking vimrc to ~/.vimrc
 ln -s $(pwd)/vimrc ~/.vimrc
 echo Linking screenrc to ~/.screenrc
 ln -s $(pwd)/screenrc ~/.screenrc
 echo Setting git configuration
-git config user.name "Matt Welch"
-git config user.email "mattw0308@gmail.com"
-git config core.editor "vim"
+git config --global user.name "Matt Welch"
+git config --global user.email "mattw0308@gmail.com"
+git config --global core.editor "vim"
 
