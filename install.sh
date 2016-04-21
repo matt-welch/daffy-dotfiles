935c19215e547b2e5d66b07c267fb1bdee3f50c0
Matt Welch <mattw0308@gmail.com>
2016-04-21 16:20:56 -0700

    feat: Update the installer to improve git config
    Added git-commit-template.txt for better commit messages and fixed the
    install script to remove 'git config' errors.
    
    See https://gist.github.com/adeekshith/cd4c95a064977cdc6c50 for template

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
- 
+# assumes the install script is executed from its home directory - not safe! 
+SRC_DIR=$(pwd) 
+
 echo Linking vimrc to ~/.vimrc
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/vim ~/.vim
+ln -s ${SRC_DIR}/vimrc ~/.vimrc
+ln -s ${SRC_DIR}/vim ~/.vim
 
 echo Linking screenrc to ~/.screenrc
-ln -s $(pwd)/screenrc ~/.screenrc
+ln -s ${SRC_DIR}/screenrc ~/.screenrc
 
 echo Setting git configuration
 git config --global user.name "Matt Welch"
 git config --global user.email "mattw0308@gmail.com"
 git config --global core.editor "vim"
-git config --global push.default simple
+git config --global push.default current
 git config --global diff.tool vimdiff
 git config --global merge.tool vimdiff
+git config --global commit.template ${SRC_DIR}/git-commit-template.txt
 
 # initialize submodules
 git submodule init
